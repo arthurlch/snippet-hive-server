1cfe2b82e9bca11261593f5956ba1a7d495b6b1d
arthurlch <lachat.arthur74@gmail.com>
2023-02-19 16:35:14 +0900

    chores: clean up create snippet func

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// handle 404
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("test"))
+
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +21,13 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	// if the method is not a POST method then we send back a 405 Not Allowed
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("snippet creation"))
 }
 
