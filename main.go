f8f21edcae61c1c2f2e2256536239bad7b9b9d22
arthurlch <lachat.arthur74@gmail.com>
2023-02-19 15:53:22 +0900

    chores: add func to handle create snippets and view snippets, base onyl

--- main.go
+++ main.go
@@ -9,10 +9,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("test"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("snipper creation"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("snipper creation"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	log.Println("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
