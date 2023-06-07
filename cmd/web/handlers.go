5dc06c507f0d7d01ab877b53250b3db950bc99da
arthurlch <lachat.arthur74@gmail.com>
2023-06-08 00:37:39 +0900

    chores: add logging

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,7 +6,8 @@ import (
 	"strconv"
 )
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -14,7 +15,8 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+func  (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,15 +4,40 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
+
 func main() {
+
 	addr := flag.String("addr", "4000", "HTTP Network Address")  
+
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime )
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog
+	}
+	
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Println("Starting server on port",*addr )
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
+
+	infoLog.Println("Starting server on port",*addr )
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
